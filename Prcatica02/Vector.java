13660fec316287e169d439b03a3072253cffc32d
katph <platakaterinevelyn@gmail.com>
2026-03-25 00:30:30 -0400

    x

--- Prcatica02/Vector.java
+++ Prcatica02/Vector.java
@@ -32,6 +32,10 @@ public class Vector {
     public Vector escalar(double n) {
         return new Vector(x * n, y * n, z * n);
     }
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ", " + z + ")";
+    }
 }
 
 class AlgebraVectorial{
@@ -41,18 +45,20 @@ class AlgebraVectorial{
         return Math.abs(suma.magnitud() - resta.magnitud()) < 1e-6;
     }
 
-    public static boolean perpendicularMutuo(Vector a, Vector b){
-        return Math.abs(a.resta(b).magnitud()- b.resta(a).magnitud()) < 1e-6;
+    public static boolean perpendicular(Vector a, Vector b, boolean mutuo){
+        double d1 = a.resta(b).magnitud();
+        double d2 = b.resta(a).magnitud();
+        return Math.abs(d1 - d2) < 1e-6;
     }
 
-    public static boolean  perpendicularOrto(Vector a, Vector b, int c){
+    public static boolean perpendicular(Vector a, Vector b, int c){
         if (c == 2) { 
             return Math.abs(a.producP(b)) < 1e-6;
     }
         return false;
     }
 
-    public static boolean perpendicularPita(Vector a, Vector b){
+    public static boolean perpendicular(Vector a, Vector b, double d){
         double Suma = a.suma(b).magnitud(); 
         double ma = a.magnitud();
         double mb = b.magnitud();
@@ -67,7 +73,7 @@ class AlgebraVectorial{
         double rz = a.z / b.z;
         return Math.abs(rx - ry) < 1e-6 && Math.abs(ry - rz) < 1e-6;
     }
-    public static boolean paralelaCruz(Vector a, Vector b) {
+    public static boolean paralela(Vector a, Vector b, String metodo) {
         Vector resultado = a.producC(b); 
         return resultado.magnitud() < 1e-6; 
     }
@@ -87,18 +93,29 @@ class AlgebraVectorial{
 class mainVector {
     public static void main(String[] args) {
         Vector v1 = new Vector(2, 3, 1);
-        Vector v2 = new Vector(4, -2, 0);
+        Vector v2 = new Vector(4, 2, 0);
 
         boolean resul1 = AlgebraVectorial.perpendicular(v1, v2);
         System.out.println("a) perpendiculares? " + resul1);
 
-        boolean resul2 = AlgebraVectorial.perpendicularOrto(v1, v2, 2);
-        System.out.println("c) perpendiculares ? " + resul2);
+        boolean resulMutuo = AlgebraVectorial.perpendicular(v1, v2, true);
+        System.out.println("b) perpendicularesMut? " + resulMutuo);
+
+        boolean resul2 = AlgebraVectorial.perpendicular(v1, v2, 2);
+        System.out.println("c) perpendicularesOrt? " + resul2);
+
+        boolean resulPita = AlgebraVectorial.perpendicular(v1, v2, 1.0);
+        System.out.println("d) perpendicularesPita? " + resulPita);
 
         Vector v3 = new Vector(1, 2, 3);
         Vector v4 = new Vector(2, 4, 6);
         System.out.println("e) Paralelas? " + AlgebraVectorial.paralela(v3, v4));
         
+        System.out.println("f) ParalelasCz? " + AlgebraVectorial.paralela(v3, v4, "cruz"));
+
+        Vector proy = AlgebraVectorial.proyeccion(v1, v2);
+        System.out.println("g) Proyección de v1 en v2: " + proy);
+
         double comp = AlgebraVectorial.componente(v1, v2);
         System.out.println("h) Componente de v1 en v2: " + comp);
 
